36354e1787197a5081af3e86278c20483ac75032
iamitpal <amitpalinfo@yahoo.in>
2023-01-09 13:25:16 +0530

    vercel

--- pages/experience.js
+++ pages/experience.js
@@ -1,7 +1,37 @@
 import React from "react";
+import Image from "next/image";
+import profile from "../public/profile.png";
+import Link from "next/link";
 
-const Experience = () => {
-  return <div>Experience</div>;
+const Experience = ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <Link href={data.html_url}>
+        <Image
+          style={{ borderRadius: "50%", marginTop: "10px" }}
+          src={profile}
+          width={120}
+          height={130}
+          priority
+          alt={"avatar"}
+        />
+      </Link>
+      <Link href={data.html_url}>
+        <h2>{data.name}</h2>
+      </Link>
+      <h2>Experience: 1200+ Hours of Coding experience at Masai School</h2>
+    </div>
+  );
 };
 
+export async function getStaticProps() {
+  const response = await fetch(`https://api.github.com/users/iamitpal`);
+  let data = await response.json();
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
 export default Experience;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import profile from "../public/profile.png";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,16 +11,20 @@ export default function Home({ data }) {
   // console.log(data);
   return (
     <div>
-      <Image
-        style={{ borderRadius: "50%", marginTop: "10px" }}
-        src={profile}
-        width={120}
-        height={130}
-        priority
-        alt={"avatar"}
-      />
-      <h2>{data.name}</h2>
-      <p>@{data.name}</p>
+      <Link href={data.html_url}>
+        <Image
+          style={{ borderRadius: "50%", marginTop: "10px" }}
+          src={profile}
+          width={120}
+          height={130}
+          priority
+          alt={"avatar"}
+        />
+      </Link>
+      <Link href={data.html_url}>
+        <h2>{data.name}</h2>
+        <p>@{data.name}</p>
+      </Link>
       <h3>{data.bio}</h3>
       <h4>Education:{"Full Stack Web-Developement (Masai School)"}</h4>
       <h4>
@@ -28,7 +33,7 @@ export default function Home({ data }) {
           "Full-stack Developer | FOSS | JavaScript | Typescript | NodeJS | Deno | ReactJS | HTML | CSS | Chakra-UI | MUI"
         }
       </h4>
-      <a href="https://drive.google.com/file/d/1_2s9ErkRxVp4JTG9P0QMX2Qi2gfoStfl/view?usp=share_link">
+      <Link href="https://drive.google.com/file/d/1_2s9ErkRxVp4JTG9P0QMX2Qi2gfoStfl/view?usp=share_link">
         <button
           style={{
             backgroundColor: "royalblue",
@@ -38,7 +43,7 @@ export default function Home({ data }) {
         >
           Resume
         </button>
-      </a>
+      </Link>
     </div>
   );
 }
